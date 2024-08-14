e40ea22b00cbc763b5abd109444275571474e821
davipythonweb <galaxys3tab32@gmail.com>
2024-08-14 01:51:14 -0300

    create metodo transaction e schemas

--- payments/api.py
+++ payments/api.py
@@ -1,10 +1,22 @@
 from ninja import Router
+from .schemas import TransactionSchema
+from django.shortcuts import get_object_or_404
+
+from users.models import User
 
 payments_router = Router()
 
-@payments_router.post('/')
-def transaction(request):
-    return {1: 1}
+@payments_router.post('/', response={200: dict, 400:dict})
+def transaction(request, transaction: TransactionSchema):
+    payer = get_object_or_404(User, id=transaction.payer)
+    payee = get_object_or_404(User, id=transaction.payee)
 
 
-#  tempo do video 59:14
+    if payer.amount < transaction.amount:
+        return 400, {'error': 'Saldo Insuficiente.'}
+
+    # print(transaction.dict())
+    return 200, {'transaction_id': 1}
+
+
+#  tempo do video 1:13:18
