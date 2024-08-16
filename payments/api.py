2fcba2151667da65401f2743fc21a8a85cdb25f8
davipythonweb <galaxys3tab32@gmail.com>
2024-08-15 23:39:57 -0300

    create regra para transferencias

--- payments/api.py
+++ payments/api.py
@@ -4,9 +4,12 @@ from django.shortcuts import get_object_or_404
 
 from users.models import User
 
+from rolepermissions.checkers import has_permission
+
+
 payments_router = Router()
 
-@payments_router.post('/', response={200: dict, 400:dict})
+@payments_router.post('/', response={200: dict, 400:dict, 403: dict})
 def transaction(request, transaction: TransactionSchema):
     payer = get_object_or_404(User, id=transaction.payer)
     payee = get_object_or_404(User, id=transaction.payee)
@@ -14,9 +17,14 @@ def transaction(request, transaction: TransactionSchema):
 
     if payer.amount < transaction.amount:
         return 400, {'error': 'Saldo Insuficiente.'}
+    
 
-    # print(transaction.dict())
+    if not has_permission(payer, 'make_transfer'):
+        return 403, {'error': 'Voce não possui permissao para realizar tranferência.'}
+    
+    if not has_permission(payee, 'receive_tranfer' ):
+        return 403, {'error': 'O usuario não pode receber tranferências.'}
     return 200, {'transaction_id': 1}
 
 
-#  tempo do video 1:13:18
+#  tempo do video  1:16:14
